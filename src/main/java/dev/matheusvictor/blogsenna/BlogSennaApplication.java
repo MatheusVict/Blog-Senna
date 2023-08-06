5d95be76e91c8bbca5ae73823fb5d7a97d8137d1
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-06 16:30:41 -0300

    intial commit

---
+++ src/main/java/dev/matheusvictor/blogsenna/BlogSennaApplication.java
@@ -0,0 +1,13 @@
+package dev.matheusvictor.blogsenna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSennaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSennaApplication.class, args);
+	}
+
+}
