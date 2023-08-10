0730138693e594eb27ade50e2c06c3fb1fe2d9f4
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-10 15:08:57 -0300

    cors

--- src/main/java/dev/matheusvictor/blogsenna/BlogSennaApplication.java
+++ src/main/java/dev/matheusvictor/blogsenna/BlogSennaApplication.java
@@ -1,9 +1,12 @@
 package dev.matheusvictor.blogsenna;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url")})
 @SpringBootApplication
 public class BlogSennaApplication {
 
